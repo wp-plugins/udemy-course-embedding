10306d0045b6a774754a26055114c47321b79272
flippz <flippz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-03 22:14:30 +0000

    fixing bug
    git-svn-id: https://plugins.svn.wordpress.org/udemy-course-embedding/trunk@392882 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.udemy.com
 Description: Adds button to editor making it easy to embed a Udemy course.
 Author: Udemy
 Version: 1.0
-Author URI: twitter.com/jackflips
+Author URI: http://www.udemy.com
 */
 
 add_filter('mce_external_plugins', "tinyplugin_register");
@@ -19,7 +19,7 @@ function tinyplugin_add_button($buttons)
 
 function tinyplugin_register($plugin_array)
 {
-    $url = trim(get_bloginfo('url'), "/")."/wp-content/plugins/tiny-plugin/editor_plugin.js";
+    $url = trim(get_bloginfo('url'), "/")."/wp-content/plugins/udemy-course-embedding/editor_plugin.js";
 
     $plugin_array['embed'] = $url;
     return $plugin_array;
